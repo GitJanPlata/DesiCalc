9db8e155ebbc99861b26de084e28e9ff7e01d318
GitJanPlata <96839905+GitJanPlata@users.noreply.github.com>
2023-07-04 20:35:25 +0200

    Prioridad añadida sobre los operadores * y /

--- calculadora-angular/src/app/calculadora/calculadora.component.ts
+++ calculadora-angular/src/app/calculadora/calculadora.component.ts
@@ -32,36 +32,71 @@ export class CalculadoraComponent {
 
   calculate() {
     this.operands.push(Number(this.display));
-
+  
+    // Primero maneja las multiplicaciones y divisiones
+    for (let i = 0; i < this.operators.length; i++) {
+      if (this.operators[i] === '*' || this.operators[i] === '/') {
+        let leftOperand = this.operands[i];
+        let operator = this.operators[i];
+        let rightOperand = this.operands[i + 1];
+  
+        if (leftOperand === undefined || rightOperand === undefined) {
+          this.display = 'Error';
+          this.operands = [];
+          this.operators = [];
+          return;
+        }
+  
+        switch(operator) {
+          case '*':
+            this.operands[i] = leftOperand * rightOperand;
+            break;
+          case '/':
+            if(rightOperand === 0) {
+              this.display = 'Error';
+              this.operands = [];
+              this.operators = [];
+              return;
+            }
+            this.operands[i] = leftOperand / rightOperand;
+            break;
+        }
+  
+        this.operands.splice(i + 1, 1); // Elimina el operando que ya se ha utilizado
+        this.operators.splice(i, 1); // Elimina el operador que ya se ha utilizado
+        i--; // Ajusta el índice para tener en cuenta los elementos que se han eliminado
+      }
+    }
+  
+    // Luego maneja las sumas y restas
     while (this.operators.length > 0) {
       let leftOperand = this.operands.shift();
       let operator = this.operators.shift();
       let rightOperand = this.operands[0];
-
-      if (leftOperand !== undefined && rightOperand !== undefined) {
-        switch(operator) {
-          case '+':
-            this.operands[0] = leftOperand + rightOperand;
-            break;
-          case '-':
-            this.operands[0] = leftOperand - rightOperand;
-            break;
-          case '*':
-            this.operands[0] = leftOperand * rightOperand;
-            break;
-          case '/':
-            this.operands[0] = leftOperand / rightOperand;
-            break;
-        }
+  
+      if (leftOperand === undefined || rightOperand === undefined) {
+        this.display = 'Error';
+        this.operands = [];
+        this.operators = [];
+        return;
+      }
+  
+      switch(operator) {
+        case '+':
+          this.operands[0] = leftOperand + rightOperand;
+          break;
+        case '-':
+          this.operands[0] = leftOperand - rightOperand;
+          break;
       }
     }
-
+  
     if (this.operands[0] !== undefined) {
       this.display = String(this.operands[0]);
     } else {
       this.display = 'Error';
     }
-
+  
     this.operands = [];
   }
 
